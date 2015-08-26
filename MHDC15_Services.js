eb3a84559d196f6aed2b056dff046d09f5a7e468
Razhil <Razhil@users.noreply.github.com>
2015-08-26 15:47:22 +0200

    Remove unused services + first draft for saving data

--- MHDC15_Services.js
+++ MHDC15_Services.js
@@ -124,98 +124,32 @@ angular.module('MHDC15App')
 		)
 	}
 	
+	var loadPlayerHero = function() {
+		alert("TODO");
+	}
+	
+	var savePlayerHero = function() {
+		var url = "https://api.mongolab.com/api/1/databases/mhdc_db/collections/Slot5?apiKey=aEDoJR0l_r7yjOT9w9tJ3WpgN0fi4jJ_";
+		
+		var output = new Object();
+		
+		output.name = $hero.name;
+		output.items = $items.getAll();
+		output.synergies = $hero.getActiveSynergies();
+		output.skills = $hero.getSkillsLevel();
+		alert(JSON.stringify(output));
+	}
+	
 	return {
 		loadHeroStats : loadHeroStats,
 		loadHeroSkills : loadHeroSkills,
 		loadHeroSynergies : loadHeroSynergies,
-		loadItems: loadItems
+		loadItems : loadItems,
+		loadPlayerHero : loadPlayerHero,
+		savePlayerHero : savePlayerHero
 	};
 })
 .factory('excelService', function($http, $hero, $items) {
-	var loadHeroStats = function(heroName) {
-		var url = "https://spreadsheets.google.com/feeds/list/1pNRoV5zKlGfvh4WKO8_TbU2c_7SRQkj4tXPYrr0pZhE/od6/public/full?alt=json&sq=hero%3D"+heroName;
-		return $http.get(url).then(
-			function(resp) {
-				if (resp.data.feed.openSearch$totalResults.$t > 0) {
-					resp.data.feed.entry.forEach(function(entry) {
-						/*hero			:	entry.gsx$hero.$t, 
-						name			:	entry.gsx$name.$t, */
-						$hero.baseStats.durability = parseInt(entry.gsx$durability.$t);
-						$hero.baseStats.strength = parseInt(entry.gsx$strength.$t);
-						$hero.baseStats.fighting = parseInt(entry.gsx$fighting.$t);
-						$hero.baseStats.speed = parseInt(entry.gsx$speed.$t);
-						$hero.baseStats.energy = parseInt(entry.gsx$energy.$t);
-						$hero.baseStats.intelligence = parseInt(entry.gsx$intelligence.$t);
-						/*dmgRat			:	parseFloat(entry.gsx$dmgrat.$t),
-						dmgRat_melee	:	parseFloat(entry.gsx$dmgratmelee.$t),*/	
-					});
-					return "OK";
-				}
-			},
-			function(resp) {
-				alert("Failed to load data : " + resp);
-				return [];
-			}
-		);
-	}
-	
-	var loadHeroSynergies = function() {
-		var url = "https://spreadsheets.google.com/feeds/list/1titE_7MhZYN8D1U-Rc9Pb80f8w9R1c649XhOpNbVbW0/od6/public/full?alt=json";
-		return $http.get(url).then(
-			function(resp) {
-				if (resp.data.feed.openSearch$totalResults.$t > 0) {
-					resp.data.feed.entry.forEach(function(entry) {
-						$hero.addSynergy(
-							entry.gsx$name.$t,
-							{name: entry.gsx$stat25name.$t, value: parseInt(entry.gsx$stat25value.$t)},
-							{name: entry.gsx$stat50name.$t, value: parseInt(entry.gsx$stat50value.$t)}
-						);
-					});
-					return "OK";
-				}
-			},
-			function(resp) {
-				alert("Failed to load data : " + resp);
-				return [];
-			}
-		)
-	}
-	
-	var loadHeroSkills = function(heroName) {
-		var url = "https://spreadsheets.google.com/feeds/list/1qlRMZw0AoxrVL5Pp9qLaBt-_v92WrTC6jFZrHun4d4M/od6/public/full?alt=json&sq=heroname%3D"+heroName;
-		return $http.get(url).then(
-			function(resp) {
-				if (resp.data.feed.openSearch$totalResults.$t > 0) {
-					resp.data.feed.entry.forEach(function(entry) {
-						var skill;
-						$hero.skills.forEach(function(heroSkill) {
-							if (heroSkill.name == entry.gsx$skillname.$t) {
-								skill = heroSkill;
-							}
-						});
-						if (!skill) {
-							skill = $hero.addSkill(entry.gsx$skillname.$t, 20, entry.gsx$tree.$t, []);
-						}
-						
-						if (entry.gsx$effect.$t == "ActiveEffect") {
-							skill.addActiveEffect((entry.gsx$effectname.$t ? entry.gsx$effectname.$t : 'Attack'), parseFloat(entry.gsx$lvl1mindmg.$t), parseFloat(entry.gsx$lvl1maxdmg.$t), 
-								parseFloat(entry.gsx$baseas.$t), parseFloat(entry.gsx$procrate.$t), entry.gsx$damagetype.$t, entry.gsx$proximity.$t, entry.gsx$extratags.$t);
-						} else if (entry.gsx$effect.$t == "PassiveEffect") {
-							skill.addPassiveEffect(entry.gsx$name.$t, parseFloat(entry.gsx$value.$t), entry.gsx$scope.$t);
-						} else if (entry.gsx$effect.$t == "SpecialEffect") {
-							skill.addSpecialEffect(entry.gsx$name.$t, entry.gsx$name.$t, entry.gsx$scope.$t);
-						}
-					});
-					return "OK";
-				}
-			},
-			function(resp) {
-				alert("Failed to load data : " + resp);
-				return [];
-			}
-		);
-	}
-	
 	var loadPlayerHero = function(user) {
 		var url = "https://spreadsheets.google.com/feeds/list/1n1i4GvdwohX3pwR3z9pQEGwrcsdev3l3YCn4ky11dP4/od6/public/full?alt=json&sq=user%3D"+user+"%20and%20heroname%3D"+$hero.name;
 		$http.get(url)
@@ -274,9 +208,6 @@ angular.module('MHDC15App')
 	}
 	
 	return {
-		loadHeroStats : loadHeroStats,
-		loadHeroSynergies : loadHeroSynergies,
-		loadHeroSkills : loadHeroSkills,
 		loadPlayerHero : loadPlayerHero
 	};
 })
